9b8a19e4ee78f9de2e4853c673eb7e64fc302752
Ferdinand Gouveia <147464330+bacalhaubyte@users.noreply.github.com>
2025-05-22 22:50:47 -0400

    Update tpsreporter_kb.py

--- tpsreporter_kb.py
+++ tpsreporter_kb.py
@@ -6,9 +6,9 @@ def keep_active():
     try:
         print("TPS Report Compiling... Press Ctrl+C to stop.")
         while True:
-            # Press F15 key (invisible key press that won't disrupt anything)
+            # Press F15 key (invisible key 'scrlk' press that won't disrupt your work)
             keyboard.press_and_release('f15')
-            # Random wait between 30-120 seconds
+            # Random wait between X,Y seconds
             wait_time = random.randint(4, 25)
             print(f"PC Load Letter! Next attempt in {wait_time} seconds.")
             time.sleep(wait_time)
